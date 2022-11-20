e13e16b378a3e5d269c28baa12d2359888f5eb3a
rafgomes <rafgomes@gmail.com>
2022-11-20 13:05:50 -0300

    Commit Aulas Feitas

---
+++ 09 - Operadores/Operadores/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operadores
+{
+    public partial class Form1 : Form
+    {
+        float Nota1, Nota2, Nota3, Nota4, media;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Nota1 = float.Parse(txtNota1.Text);
+            Nota2 = float.Parse(txtNota2.Text);
+            Nota3 = float.Parse(txtNota3.Text);
+            Nota4 = float.Parse(txtNota4.Text);
+            media = (Nota1 + Nota2 + Nota3 + Nota4) / 4;
+
+            txtMedia.Text = Convert.ToString(media);
+
+            //Situação
+            if (media >= 7) 
+            {
+                lblStatus.Text = "APROVADO";            
+            } else
+            {
+                lblStatus.Text = "REPROVADO";
+            }
+
+        }
+    }
+}
---
+++ 10 - Switch Case e Op Ternario/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Switch_Case_e_Op_Ternario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOpTer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(74, 28);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 0;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(104, 66);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(83, 95);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Op. Ternario:";
+            // 
+            // lblOpTer
+            // 
+            this.lblOpTer.AutoSize = true;
+            this.lblOpTer.Location = new System.Drawing.Point(89, 170);
+            this.lblOpTer.Name = "lblOpTer";
+            this.lblOpTer.Size = new System.Drawing.Size(0, 13);
+            this.lblOpTer.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 271);
+            this.Controls.Add(this.lblOpTer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOpTer;
+    }
+}
+
