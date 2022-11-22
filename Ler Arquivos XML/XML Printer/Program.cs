5ed8ba98913ba61b069ee87041d9965c7b251abf
rafgomes <rafgomes@gmail.com>
2022-11-22 01:33:14 -0300

    Leitor de XML

---
+++ Ler Arquivos XML/XML Printer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML_Printer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("C:\\Projets\\Lessons\\Ler Arquivos XML\\ABCConfig.xml");
+
+            XmlNode vHeith = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Height");
+            XmlNode vWidth = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Width");
+            XmlNode vX = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/X");
+            XmlNode vY = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Y");
+
+            string H = vHeith.InnerText;
+            string W = vWidth.InnerText;
+            string X = vX.InnerText;
+            string Y = vY.InnerText;
+
+            Console.WriteLine($"Height = {H} / Width = {W} / X = {X} / Y = {Y}");
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
