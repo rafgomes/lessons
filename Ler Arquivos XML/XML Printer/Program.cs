fcc090718540d83c6ebb962cb301d48c9b597f54
rafgomes <rafgomes@gmail.com>
2022-11-22 22:45:13 -0300

    Read List XML

--- Ler Arquivos XML/XML Printer/Program.cs
+++ Ler Arquivos XML/XML Printer/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Serialization;
+
 
 namespace XML_Printer
 {
@@ -13,25 +17,48 @@ namespace XML_Printer
     {
         static void Main(string[] args)
         {
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load("C:\\Projets\\Lessons\\Ler Arquivos XML\\ABCConfig.xml");
+            //XmlDocument xmlDoc = new XmlDocument();
+            //xmlDoc.Load(@"C:\Projets\Lessons\Ler Arquivos XML\ABCConfig.xml");
 
-            XmlNode vHeith = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Height");
-            XmlNode vWidth = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Width");
-            XmlNode vX = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/X");
-            XmlNode vY = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Y");
+            //XmlNode vHeith = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Height");
+            //XmlNode vWidth = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Width");
+            //XmlNode vX = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/X");
+            //XmlNode vY = xmlDoc.DocumentElement.SelectSingleNode("/ABCConfig/ImageDummyClass/Y");
 
-            string H = vHeith.InnerText;
-            string W = vWidth.InnerText;
-            string X = vX.InnerText;
-            string Y = vY.InnerText;
+            //string H = vHeith.InnerText;
+            //string W = vWidth.InnerText;
+            //string X = vX.InnerText;
+            //string Y = vY.InnerText;
 
-            Console.WriteLine($"Height = {H} / Width = {W} / X = {X} / Y = {Y}");
+            //Console.WriteLine($"Height = {H} / Width = {W} / X = {X} / Y = {Y}");
 
+
+            //Console.ReadLine();
+
+            //=================================================================================================
+
+            XmlDocument xmlAlbum = new XmlDocument();
+            xmlAlbum.Load(@"C:\Projets\Lessons\Ler Arquivos XML\Album.xml");
+
+            XmlNodeList lista;
+            XmlElement root = xmlAlbum.DocumentElement;
+            lista = root.SelectNodes("/Album/Tracks/Track");
+            foreach (XmlNode faixa in lista)
+            {
+                string trackName = faixa.Attributes["name"].Value;
+                string path = faixa.SelectSingleNode("Path").InnerText;
+
+                Console.WriteLine($"Faixa: {trackName} em {path}");
+
+                //string mesmaTrackName = faixa.SelectSingleNode("@name").Value;
+                //Console.WriteLine($"Faixa: {faixa} em {path}");
+
+            }
+
+
+    
 
             Console.ReadLine();
-
-
         }
     }
 }
