97cb14e4915ea38e583dd2e247b381d24cd3ab2a
rafgomes <rafgomes@gmail.com>
2022-11-22 22:50:59 -0300

    Send XML file

---
+++ 04 - Classes/LearnClasses/NomeCompleto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LearnClasses
+{
+    internal class NomeCompleto
+    {
+        public string nome;
+        public string sobrenome;
+
+        public void NomeInteiro()
+        {
+            MessageBox.Show(Resposta());
+        }
+
+        private string Resposta()
+        {
+            string completo = nome + " " + sobrenome;
+            return completo;
+        }
+    }
+}
