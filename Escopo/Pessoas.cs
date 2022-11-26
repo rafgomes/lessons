beb5e0d94e042f56f489f87911f50dc38bb25eb0
rafgomes <rafgomes@gmail.com>
2022-11-26 20:32:55 -0300

    Aula Escopo

---
+++ Escopo/Pessoas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaEscopo //a classe Pessoas consegue acesar a classe Program, pois pertencem ao mesmo namespace
+{
+    internal class Pessoas
+    {
+    }
+}
