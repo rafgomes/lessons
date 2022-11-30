96b7e2ad7f79a0214b157825763026ba8189f0f8
rafgomes <rafgomes@gmail.com>
2022-11-30 20:15:03 -0300

    Matematica

---
+++ Matematica/Matematica.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public class Matematica
+    {
+        public int num1;
+        public int num2;
+        public int result;
+        public double resultDiv;
+        
+        public int Soma()
+        {
+            result = num1 + num2;
+            return result;
+        }
+
+        public int Sub()
+        {
+            result = num1 - num2;
+            return result;
+        }
+
+        public int Mult()
+        {
+            result = num1 * num2;
+            return result;
+        }
+
+        public double Div()
+        {
+            if(num1 == 0 || num2 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                resultDiv = (double)num1 / (double)num2;
+                return resultDiv;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ Matematica/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematica matematica = new Matematica();
+            
+            Console.WriteLine("Digite o Primeiro Numero: ");
+            int a = int.Parse(Console.ReadLine());
+            matematica.num1 = a;
+
+            Console.WriteLine("Digite o Segundo Numero: ");
+            int b = int.Parse(Console.ReadLine());
+            matematica.num2 = b;
+
+            Console.WriteLine("");
+            Console.WriteLine($"Soma: {matematica.Soma()}");
+            Console.WriteLine($"Subtração: {matematica.Sub()}");
+            Console.WriteLine($"Multiplicação: {matematica.Mult()}");
+            Console.WriteLine($"Divisão: {matematica.Div()}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
