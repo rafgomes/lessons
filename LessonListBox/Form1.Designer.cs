5aca1f4573dcc4dc7897aa492b51440aa5da1e23
rafgomes <rafgomes@gmail.com>
2022-12-03 19:51:32 -0300

    Listbox With SQL

--- LessonListBox/Form1.Designer.cs
+++ LessonListBox/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.tbName.Name = "tbName";
             this.tbName.Size = new System.Drawing.Size(179, 20);
             this.tbName.TabIndex = 0;
+            this.tbName.TextChanged += new System.EventHandler(this.tbName_TextChanged);
             // 
             // lbNames
             // 
--- LessonListBox/Form1.cs
+++ LessonListBox/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http.Headers;
@@ -12,45 +13,99 @@ using System.Windows.Forms;
 namespace LessonListBox
 {
     public partial class Form1 : Form
-    {
+    {   
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        
         public Form1()
         {
             InitializeComponent();
+            conn.ConnectionString = "Server=.;Database=BancoTeste;Trusted_Connection=True;";
         }
 
         List<string> listaDeNames = new List<string>();
 
+        private void GetData()
+        {
+           try
+            {
+                string query = "SELECT Name FROM ListBoxNames"; //carrega os dados da tabela para o listbox
+                cmd = new SqlCommand("SELECT Name FROM ListBoxNames", conn); //instancias novos comandos no SQL
+                cmd.CommandText = query;
+                da = new SqlDataAdapter();
+                da.SelectCommand = cmd; //busca os dados no banco
+                dt = new DataTable();
+                da.Fill(dt); //atualiza as linhas
+                lbNames.DataSource = dt;
+                lbNames.DisplayMember = "Name";
+                listaDeNames = dt.Rows.OfType<DataRow>().Select(dr => dr.Field<string>("Name")).ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                da.Dispose();
+            }
+        }
+
         public void Form1_Load(object sender, EventArgs e)
         {
+            GetData();
+            
             btnRemove.Enabled = false;
         }
 
+        
         public void btnAdd_Click(object sender, EventArgs e)
         {
-            if(tbName.Text == "")
+            string listboxnames = tbName.Text;
+            if(listboxnames == "")
             {
                 MessageBox.Show("Digite um nome!");
                 tbName.Focus();
             }
             else
             {
-                listaDeNames.Add(tbName.Text);
+                string sqlinsert = $@"INSERT INTO [dbo].[ListBoxNames] ([Name]) VALUES ('{listboxnames}')";
+                listaDeNames.Add(listboxnames);
                 tbName.Clear();
                 lbNames.DataSource = null;
                 lbNames.DataSource = listaDeNames;
-            }
+
+                using (cmd = new SqlCommand(sqlinsert, conn))
+                { 
+                    conn.Open();
+                    //cmd.CommandText = sqlinsert;
+                    cmd.ExecuteNonQuery();
+                }
+            } 
         }
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
-            if(lbNames.SelectedIndex > -1)
+            int listboxnames = (lbNames.SelectedIndex);
+            string nome = lbNames.Text;
+            if (lbNames.SelectedIndex > -1)
             {
-                listaDeNames.RemoveAt(lbNames.SelectedIndex);
+                string sqldelete = $@"DELETE FROM [dbo].[ListBoxNames] WHERE ([Name]) = ('{nome}')";
+                listaDeNames.RemoveAt(listboxnames);
                 lbNames.DataSource = null;
                 lbNames.DataSource = listaDeNames;
+
+                using (cmd = new SqlCommand(sqldelete, conn))
+                {
+                    conn.Open();
+                    //cmd.CommandText = sqldelete;
+                    cmd.ExecuteNonQuery();
+                }
             }
             else
             {
+
                 MessageBox.Show("Selecione um Nome!");
             }
         }
@@ -67,12 +122,13 @@ namespace LessonListBox
 
         private void lbNames_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(lbNames.SelectedIndex == -1) { 
-                btnRemove.Enabled = false;
+            if (lbNames.SelectedIndex > -1)
+            {
+                btnRemove.Enabled = true;
             }
             else
             {
-                btnRemove.Enabled = true;   
+                btnRemove.Enabled = false;
             }
         }
 
@@ -93,6 +149,11 @@ namespace LessonListBox
                 lbNames.DataSource = null;
                 lbNames.DataSource = armazenaResultado;
             }
+        }
+
+        private void tbName_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
