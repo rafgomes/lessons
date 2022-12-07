6ae04bb9c20c10ada26edb93e71a361a5a5c4b15
rafgomes <rafgomes@gmail.com>
2022-12-07 20:50:45 -0300

    Organizar

---
+++ REVISAO/FuncoesRecursivas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncoesRecursivas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LerArquivo(@"C:\temp\Arq1.txt");
+        }
+
+        private static void LerArquivo(string nomeArquivo)
+        {
+            using (StreamReader arquivo = File.OpenText(nomeArquivo))
+            {
+                string linha;
+                while ((linha = arquivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
