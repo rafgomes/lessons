d2e9627de923cfe869074f16a03073dcbde56df1
milocaetano <camilo.s.caetano@gmail.com>
2022-12-07 20:56:07 -0300

    aprendendo interfaces junto com execute cmd

---
+++ AulaIntefaces/AprendendoMais/Commandos/DeleteArquivosCmd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaIntefaces.AprendendoMais.Commandos
+{
+    public class DeleteArquivosCmd : ICommandos
+    {
+        string toFolder;
+        public DeleteArquivosCmd(string toFolder)
+        {
+            toFolder = toFolder.ToLower();
+        }
+
+        public void ExecuteCmd()
+        {
+            Console.WriteLine($"Delete arquivos da pasta {toFolder}");
+        }
+    }
+}
