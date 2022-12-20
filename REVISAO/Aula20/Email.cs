eb1004a787b57c5d7f3fe5c413253fa18de4e1dd
rafgomes <rafgomes@gmail.com>
2022-12-20 10:21:05 -0300

    Aula 20

---
+++ REVISAO/Aula20/Email.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20
+{
+    public class Email //classe Singleton
+    { 
+        private Email()
+        {
+
+        }
+
+        private static Email instancia; //atributo instancia privado
+
+        public string Origin;
+        public string Destino; //todos os atributos publicos para fazer com que a classe Email funcione
+        public string Titulo;
+        public string Corpo;
+
+        public void EnviarEmail()
+        {
+            Console.WriteLine($"Enviado email para: {Destino}, com o titulo: {Titulo}, menssagem: {Corpo}");
+        }
+
+        public static Email Instancia //propriedade instancia publica
+        {
+            get
+            {
+                if (instancia == null) //se instancia está vazia(null)
+                {
+                    instancia = new Email(); //então cria uma nova instancia
+                }
+                return instancia;
+            }
+        }
+    
+    }
+
+}
---
+++ REVISAO/Aula20/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20
+{
+    internal class Program
+    {
+        /*
+        sealed class Animal
+        {
+            public string teste;
+        }
+        
+        class Macaco : Animal //não pode herdar uma classe selada
+        {
+            public string teste2()
+            {
+                return "sss";
+            }
+        }
+        */
+
+        /*
+        class Animal
+        {
+            public string teste;
+            public sealed string Teste2()
+            {
+                return "";
+            }
+        }
+
+        class Macaco : Animal
+        {
+            public string Teste2() //tambem não pode ser sobrescrito um metodo selado
+            {
+                return "sss";
+            }
+        }
+        */
+
+        /*
+        partial class Animal //classe partial uma faz parte da outra
+        {
+            public string teste;
+            partial void tt();
+        }
+
+        partial class Animal //quando instanciar a classe Animal, terá acesso a todos os metodos e atributos
+        {
+            public string teste2;
+            partial void tt()
+            {
+                Console.WriteLine("teste");
+            }
+        }
+        */
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Email.Instancia.Corpo = "O corpo do email aqui";
+            Email.Instancia.Titulo = "TITULO DO EMAIL";
+            Email.Instancia.Origin = "origem@email.com";
+            Email.Instancia.Destino = "destion@email.com";
+
+            Email.Instancia.EnviarEmail();
+        }
+    }
+}
