78efbbbbd7b5b608e377b15bb2f1b05d96f21baa
rafgomes <rafgomes@gmail.com>
2022-12-22 18:37:00 -0300

    lesson API Request

---
+++ Camilo/SistemaDeTarefas/SistemaDeTarefas/Data/SistemaTarefasDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data.Map;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SistemaTarefasDBContext : DbContext
+    {
+        //Está trabalhando com ORM
+        
+        public SistemaTarefasDBContext(DbContextOptions<SistemaTarefasDBContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; } //representa a tabela Usuarios no banco de dados (vai criar a tabela)
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            modelBuilder.ApplyConfiguration(new TarefaMap());
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
---
+++ Camilo/SistemaDeTarefas/SistemaDeTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int ID { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+    }
+}
