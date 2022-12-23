4cd12257e7707a1340bfdc3d769cb06b441e4762
rafgomes <rafgomes@gmail.com>
2022-12-23 12:09:23 -0300

    CotacaoDolar

---
+++ CotacaoDolarHG/Currency.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace CotacaoDolarHG
+{
+    public class Currency
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "buy")]
+        public decimal  Compra { get; set; }
+        [JsonProperty(PropertyName = "sell")]
+        public decimal Venda { get; set; }
+        [JsonProperty(PropertyName = "variation")]
+        public decimal Variacao { get; set; }
+    }
+}
