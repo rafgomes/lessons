0fcce1f06fd237dbcb9632f4757e861e980a7122
rafgomes <rafgomes@gmail.com>
2022-12-24 07:14:05 -0300

    API NET CSharp

--- API NET CSharp/ConsumindoApiAluno2/Form1.cs
+++ API NET CSharp/ConsumindoApiAluno2/Form1.cs
@@ -1,37 +1,28 @@
 ﻿using ConsumindoApiAluno.Entities;
 using ConsumindoApiAluno.Entities.Services;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ConsumindoApiAluno2
 {
     public partial class Form1 : Form
     {
-        public int id;
-        public Aluno alunoEncontrado;
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private async void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            AlunoServices alunoServices = new AlunoServices();
-            alunoEncontrado = await alunoServices.Integracao(id);
-            
+
+
         }
 
-        private void btnBuscar_Click(object sender, EventArgs e)
+        private async void btnBuscar_Click(object sender, EventArgs e)
         {
-            id = int.Parse(txtBuscar.Text);
+            int id = int.Parse(txtBuscar.Text);
+            AlunoServices alunoServices = new AlunoServices();
+            Aluno alunoEncontrado = await alunoServices.Integracao(id);
 
             if (!alunoEncontrado.Verificacao)
             {
